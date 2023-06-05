00a166e9231ac395e6ad5432d72d25a0c516d4b8
Ib Helmer Nielsen <ihn@ihn.dk>
2023-06-05 17:08:26 +0200

    Add files via upload

--- InstallPSModul.ps1
+++ InstallPSModul.ps1
@@ -1,12 +1,26 @@
-# Tjek om modulet AzureRM er installeret i powershell, fjernes hvis det er installeret
+<#
+	.NOTES
+	===========================================================================
+	 Created on:   	4/6-2023
+	 Created by:   	Ib Helmer Nilsen
+	 Organization: 	Professionshøjskolen UCN
+	 Filename:     	InstallPSModul.ps1
+	 URL:			https://github.com/ibhelmer/AzureCloudKursus
+	===========================================================================
+	.DESCRIPTION
+		Klargør og installere Azure Powershell modul
+#>
+# Tjek om modulet AzureRM er installeret i PowerShell, fjernes hvis det er installeret
 Get-Module -ListAvailable | Where-Object {$_.Name -like 'AzureRM*'} | Uninstall-Module 
+
 # Dette kan også gøres "manuelt" ved først at liste installeret modul med navnet AzureRM:
 #Get-InstalledModule -name AzureRM
 # Hvis der er installeret et modul med navnet AzureRM fjernes dette:
 #Uninstall-Module -name AzureRM
+
 #Set herefter executionPolicy for powershell til RemoteSigned 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# Installer så Az PowerShell modulet i Windows PowerShell:
+# Installer Az PowerShell modulet i Windows PowerShell:
 Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 # Nu kan der forbindes til Azure via:
 Connect-AzAccount
